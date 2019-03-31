cdb9876b5947c79b92cae53d91568a2f39718bf2
katipietarinen <43537212+katipietarinen@users.noreply.github.com>
2019-03-31 22:05:28 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import scraperwiki
-html = scraperwiki.scrape('https://www.hs.fi/uusi/')
+html = scraperwiki.scrape('https://inmo.ie/6022')
 print "Click on the ...more link to see the whole page"
 print html
 
